4156087fda37ec42d1c33004a856b6ead1546992
EEifen <ethangongora24@gmail.com>
2023-01-31 20:31:50 +0100

    Ajout Mort
    Lock position si pv <= 0 + "animation" mort

--- Assets/PlayGround/HealthBar/PlayerHealth.cs
+++ Assets/PlayGround/HealthBar/PlayerHealth.cs
@@ -6,18 +6,23 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] Health healthBarRef;
+    Rigidbody2D rb;
+    Animator animController;
 
     private int maxHealth = 100;
     private int currentHealth;
-
+    
 
 
 
     // Start is called before the first frame update
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        animController= GetComponent<Animator>();
         currentHealth = maxHealth;
         healthBarRef.SetMaxHealth(maxHealth);
+
     }
 
     private void Update()
@@ -26,11 +31,22 @@ public class PlayerHealth : MonoBehaviour
         {
             TakeDamage(25);
         }
+
+        if (currentHealth == 0)
+        {
+            Death();
+        }
     }
 
     private void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBarRef.SetHealth(currentHealth);
+    }
+
+    private void Death()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        animController.SetBool("isDead", true);       
     }
 }
